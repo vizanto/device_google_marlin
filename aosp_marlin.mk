b294adad984ff20fdbe15c04240c5431a98e1849
Daniel Micay <danielmicay@gmail.com>
2016-12-22 14:37:51 -0500

    whitelist LocalTransport backup service for AOSP
    Change-Id: Ida8369c073ebc10870f609e5f6913c40282d4bdf

--- aosp_marlin.mk
+++ aosp_marlin.mk
@@ -33,6 +33,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := true
 
 PRODUCT_COPY_FILES += device/google/marlin/fstab.aosp_common:root/fstab.marlin
 
+PRODUCT_COPY_FILES += device/google/marlin/backup.xml:system/etc/sysconfig/backup.xml
+
 $(call inherit-product, device/google/marlin/device-marlin.mk)
 $(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-marlin.mk)
 
